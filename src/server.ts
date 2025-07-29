52738e51d6e0ee472e6122935f5074cbc372c9d1
Jesus Yael Padrón Grimaldo <jesusyaelpadrongrimaldo@gmail.com>
2025-07-28 23:35:05 -0600

    Se suben dependencias para configurar el cors

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,7 @@
 import express from 'express'
 import dotenv from 'dotenv'
+import cors from 'cors'
+import { corsConfig } from './config/cors'
 import { connectDB } from './config/db'
 import projectRoutes from './routes/projectRoutes'
 
@@ -7,6 +9,8 @@ dotenv.config()
 connectDB()
 
 const app = express()
+app.use(cors(corsConfig))
+
 app.use(express.json())
 
 // Mis rutas
