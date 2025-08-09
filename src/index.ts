5714f32d65826053bafaf357d3183dd4c657a06a
Jesus Yael Padrón Grimaldo <jesusyaelpadrongrimaldo@gmail.com>
2025-08-08 18:58:17 -0600

    Se cambia el puerto para que funcione

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import colors from 'colors'
 import server from './server';
 
-const port = process.env.PORT || 4000
+const port = process.env.PORT || 3000
 
 server.listen(port, ()  => {
     console.log(colors.white.italic.bold.bgGreen(`REST API Funcionando en el puerto ${port}`))
